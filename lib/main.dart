bb5327d1bec6a08614c1f19e7cc0fbd3d650a2d5
Li Boshi <boshi@robowebtech.com>
2020-10-28 11:54:21 +0800

    finish exam 2

--- lib/main.dart
+++ lib/main.dart
@@ -43,11 +43,106 @@ class _MyHomePageState extends State<MyHomePage> {
     return null;
   }
 
-  void _onChange({String columnString, String rowString}) {
+  void _onChange({String columnString = '', String rowString = ''}) {
     final int column = columnString.isNotEmpty ? int.parse(columnString) : 0;
     final int row = rowString.isNotEmpty ? int.parse(rowString) : 0;
     if (column > 0) this.column = column;
     if (row > 0) this.row = row;
+    setState(() {});
+  }
+
+  List<Widget> _buildContent(BuildContext context) {
+    List<Widget> widgets = [
+      Padding(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Text('Column: '),
+            Expanded(
+              child: TextFormField(
+                validator: this.validateNum,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                keyboardType: TextInputType.number,
+                onChanged: (column) => this._onChange(columnString: column),
+              ),
+            ),
+            SizedBox(width: 20),
+            Text('Row: '),
+            Expanded(
+              child: TextFormField(
+                validator: this.validateNum,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                keyboardType: TextInputType.number,
+                onChanged: (row) => this._onChange(rowString: row),
+              ),
+            )
+          ],
+        ),
+      ),
+    ];
+    if (this.column == 0 || this.row == 0) return widgets;
+    widgets.add(
+      Expanded(child: LayoutBuilder(builder: this._buildGridView)),
+    );
+    return widgets;
+  }
+
+  Widget _buildGridView(BuildContext context, BoxConstraints constraints) {
+    final buttonHeight = 150;
+    final width = constraints.maxWidth / column;
+    final height = (constraints.maxHeight - buttonHeight - row) / row;
+    print(height);
+    final double ratio = width / height;
+    return Container(
+      height: constraints.maxHeight,
+      child: Column(
+        children: [
+          GridView.builder(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: width,
+              crossAxisSpacing: 1,
+              mainAxisSpacing: 1,
+              childAspectRatio: ratio,
+            ),
+            itemCount: this.column * this.row,
+            itemBuilder: (context, index) {
+              return Container(
+                color: Colors.amberAccent,
+              );
+            },
+          ),
+          GridView.builder(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: width,
+              crossAxisSpacing: 1,
+              childAspectRatio: width / (buttonHeight),
+            ),
+            itemCount: this.column,
+            itemBuilder: (context, index) {
+              return Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(top: 10),
+                child: Text(
+                  '確定',
+                  style: TextStyle(color: Colors.white),
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.grey[500]),
+                  color: Colors.black45,
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -56,41 +151,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('Example'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Text('Column: '),
-                Expanded(
-                  child: TextFormField(
-                    validator: this.validateNum,
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    keyboardType: TextInputType.number,
-                    onChanged: (column) => this._onChange(columnString: column),
-                  ),
-                ),
-                SizedBox(width: 20),
-                Text('Row: '),
-                Expanded(
-                  child: TextFormField(
-                    validator: this.validateNum,
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    keyboardType: TextInputType.number,
-                    onChanged: (row) => this._onChange(rowString: row),
-                  ),
-                )
-              ],
-            ),
-          ),
-          CustomScrollView(
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            slivers: [],
-          )
-        ],
-      ),
+      body: SafeArea(child: Column(children: this._buildContent(context))),
     );
   }
 }
