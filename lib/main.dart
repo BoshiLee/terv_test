8751afcb71e3acc5ff0c179e6573df94ffe03f15
Li Boshi <boshi@robowebtech.com>
2020-10-28 09:48:04 +0800

    1. remover comments 2. add onChange handle

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,16 +21,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,6 +28,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int column = 0;
+  int row = 0;
+
   @override
   void initState() {
     super.initState();
@@ -60,6 +41,13 @@ class _MyHomePageState extends State<MyHomePage> {
     final int number = int.parse(value);
     if (number <= 0) return 'Number must bigger than 0';
     return null;
+  }
+
+  void _onChange({String columnString, String rowString}) {
+    final int column = columnString.isNotEmpty ? int.parse(columnString) : 0;
+    final int row = rowString.isNotEmpty ? int.parse(rowString) : 0;
+    if (column > 0) this.column = column;
+    if (row > 0) this.row = row;
   }
 
   @override
@@ -80,6 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     validator: this.validateNum,
                     autovalidateMode: AutovalidateMode.onUserInteraction,
                     keyboardType: TextInputType.number,
+                    onChanged: (column) => this._onChange(columnString: column),
                   ),
                 ),
                 SizedBox(width: 20),
@@ -89,10 +78,16 @@ class _MyHomePageState extends State<MyHomePage> {
                     validator: this.validateNum,
                     autovalidateMode: AutovalidateMode.onUserInteraction,
                     keyboardType: TextInputType.number,
+                    onChanged: (row) => this._onChange(rowString: row),
                   ),
                 )
               ],
             ),
+          ),
+          CustomScrollView(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            slivers: [],
           )
         ],
       ),
